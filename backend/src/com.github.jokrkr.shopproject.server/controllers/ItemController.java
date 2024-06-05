488538299adb2bbdf4b19642320a39189074560c
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-06-05 14:02:23 +0200

    Update ItemController.java
    somehow the I+temController did not  get comitted last time

--- backend/src/com.github.jokrkr.shopproject.server/controllers/ItemController.java
+++ backend/src/com.github.jokrkr.shopproject.server/controllers/ItemController.java
@@ -1,17 +1,28 @@
 package com.github.jokrkr.shopproject.server.controllers;
 
+import com.github.jokrkr.shopproject.server.handlers.CRUD.CreateItem;
+import com.github.jokrkr.shopproject.server.handlers.CRUD.DeleteItem;
+import com.github.jokrkr.shopproject.server.handlers.CRUD.ReadItem;
+import com.github.jokrkr.shopproject.server.handlers.CRUD.UpdateItem;
+import com.github.jokrkr.shopproject.server.handlers.UnsupportedMethod;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
-import java.io.OutputStream;
 
 public class ItemController implements HttpHandler {
+
     @Override
-    public void handle(HttpExchange t) throws IOException {
-        String response = "This is the response from the backend";
-        t.sendResponseHeaders(200, response.length());
-        OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+
+        HttpHandler handler = switch (method) {
+            case "GET" -> new ReadItem();
+            case "POST" -> new CreateItem();
+            case "PUT" -> new UpdateItem();
+            case "DELETE" -> new DeleteItem();
+            default -> new UnsupportedMethod();
+        };
+
+        handler.handle(exchange);
     }
 }
