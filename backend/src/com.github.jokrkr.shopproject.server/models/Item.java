46b4d2e66789d3e97461c89d00f68335d7144bc7
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-06-05 15:21:41 +0200

    Update Item.java

--- backend/src/com.github.jokrkr.shopproject.server/models/Item.java
+++ backend/src/com.github.jokrkr.shopproject.server/models/Item.java
@@ -1,14 +1,18 @@
 package com.github.jokrkr.shopproject.server.models;
 
 public class Item {
-    String name;
-    double price;
+    String _name;
+    double _price;
+    String _type;
 
-    public Item(String name, double price) {
-        this.name = name;
-        this.price = price;
+
+    public Item(String type, String name, double price) {
+        this._name = name;
+        this._price = price;
+        this._type = type;
     }
 
-    public String getName() {return name;}
-    public double getPrice() {return price;}
+    public String getName() {return _name;}
+    public double getPrice() {return _price;}
+    public String getType() {return _type;}
 }
