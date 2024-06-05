6797b3b3afed2510758fe5940b6e6f00a720d9cc
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-06-05 14:10:58 +0200

    Update UnsupportedMethod.java
    in case switch defaults

--- backend/src/com.github.jokrkr.shopproject.server/handlers/UnsupportedMethod.java
+++ backend/src/com.github.jokrkr.shopproject.server/handlers/UnsupportedMethod.java
@@ -2,12 +2,16 @@ package com.github.jokrkr.shopproject.server.handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class UnsupportedMethod implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-
+        String response = "Method not supported";
+        exchange.sendResponseHeaders(405, response.length());
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes());
+        }
     }
 }
