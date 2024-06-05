c6fa7c3e0869b2fcdad0cfa4368457f0c21b86ff
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-06-05 14:50:25 +0200

    Item.java
    basic item class  with name and price.

---
+++ backend/src/com.github.jokrkr.shopproject.server/models/Item.java
@@ -0,0 +1,14 @@
+package com.github.jokrkr.shopproject.server.models;
+
+public class Item {
+    String name;
+    double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {return name;}
+    public double getPrice() {return price;}
+}
