b6ade4835b8b80fb8415a74c2dbc2d444c244e23
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-06-24 11:17:48 +0200

    update Item class
    updating class so that it can be better utilized

--- backend/src/com.github.jokrkr.shopproject.server/models/Item.java
+++ backend/src/com.github.jokrkr.shopproject.server/models/Item.java
@@ -1,17 +1,27 @@
 package com.github.jokrkr.shopproject.server.models;
 
 public class Item {
-   private final String _name;
-   private final double _price;
-   private final String _type;
+    private final String _name;
+    private final double _price;
+    private final String _type;
+    private final int _quantity;
+    private final double _value;
 
-    public Item(String type, String name, double price) {
+    public Item(String type, String name, double price, int quantity, double value) {
         this._name = name;
         this._price = price;
         this._type = type;
+        this._quantity = quantity;
+        this._value = value;
     }
 
-    public String getName() {return _name;}
-    public double getPrice() {return _price;}
     public String getType() {return _type;}
+
+    public String getName() {return _name;}
+
+    public double getPrice() {return _price;}
+
+    public int getQuantity() {return _quantity;}
+
+    public double getValue() {return _value;}
 }
