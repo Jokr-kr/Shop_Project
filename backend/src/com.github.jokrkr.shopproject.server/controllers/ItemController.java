241d70c57e2ef8019098958b88b0b0344cf1a058
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-14 18:05:57 +0200

    Update ItemController.java
    adding comments and changing imports to reflect the new directory

--- backend/src/com.github.jokrkr.shopproject.server/controllers/ItemController.java
+++ backend/src/com.github.jokrkr.shopproject.server/controllers/ItemController.java
@@ -1,10 +1,10 @@
 package com.github.jokrkr.shopproject.server.controllers;
 
-import com.github.jokrkr.shopproject.server.handlers.CRUD.CreateItem;
-import com.github.jokrkr.shopproject.server.handlers.CRUD.ReadItem;
-import com.github.jokrkr.shopproject.server.handlers.CRUD.UpdateItem;
-import com.github.jokrkr.shopproject.server.handlers.CRUD.DeleteItem;
-import com.github.jokrkr.shopproject.server.handlers.UnsupportedMethod;
+import com.github.jokrkr.shopproject.server.CRUD.Item.CreateItem;
+import com.github.jokrkr.shopproject.server.CRUD.Item.ReadItem;
+import com.github.jokrkr.shopproject.server.CRUD.Item.UpdateItem;
+import com.github.jokrkr.shopproject.server.CRUD.Item.DeleteItem;
+import com.github.jokrkr.shopproject.server.CRUD.UnsupportedMethod;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -15,6 +15,8 @@ public class ItemController implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
+        //------------------
+        //this selects which handler to use
         HttpHandler handler = switch (method) {
             case "GET" -> new ReadItem();
             case "POST" -> new CreateItem();
@@ -22,7 +24,8 @@ public class ItemController implements HttpHandler {
             case "DELETE" -> new DeleteItem();
             default -> new UnsupportedMethod();
         };
-
+        //------------------
+        //gives the exchange to the selected handler
         handler.handle(exchange);
     }
 }
