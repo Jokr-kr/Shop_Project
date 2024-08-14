6807a5632966ac08f3cff51aa743e63a19b8152b
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-14 12:51:47 +0200

    Update User.java
    adding hash to passwords and changing to the role handling system rather than the Admin == true/false system

--- backend/src/com.github.jokrkr.shopproject.server/models/User.java
+++ backend/src/com.github.jokrkr.shopproject.server/models/User.java
@@ -1,20 +1,36 @@
 package com.github.jokrkr.shopproject.server.models;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
 public class User {
     private final String _userName;
-    private final String _password;
-    private boolean _admin;
-    private final String _role;
+    private final String _passwordHash;
+    private final Role _role;
 
-    public User(String UserName, String Password, boolean Admin, String Role) {
+    public User(String UserName, String Password, boolean Admin, Role Role) {
         this._userName = UserName;
-        this._password = Password;
+        this._passwordHash = HashPassword(Password);
         this._role = Role;
-        if (Role.equals("Admin")) {_admin = true;}
     }
+
+    private String HashPassword(String password) {
+        try {
+ MessageDigest md =MessageDigest.getInstance("SHA-256");
+ byte[] hash = md.digest(password.getBytes());
+ StringBuilder hexString = new StringBuilder();
+ for (byte b : hash) {
+     hexString.append(String.format("%02X", b));
+ }
+ return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("error while hashing password", e);
+        }
+    }
+
     public String getUserName() {return _userName;}
-    public String getPassword() {return _password;}
-    public boolean isAdmin() {return _admin;}
-    public String getRole() {return _role;}
+    public String getPassword() {return _passwordHash;}
+    public Role getRole() {return _role;}
 
 }
