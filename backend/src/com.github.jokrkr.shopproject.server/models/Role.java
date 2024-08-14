2ffd53a154aa8cb8814be0e8449729b0c749cf64
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-14 12:50:10 +0200

    role enum
    handles different roles in the company

---
+++ backend/src/com.github.jokrkr.shopproject.server/models/Role.java
@@ -0,0 +1,16 @@
+package com.github.jokrkr.shopproject.server.models;
+
+public enum Role {
+    regular(0),
+    moderator(1),
+    admin(2);
+
+    private final int _level;
+
+    Role(int level) {
+        this._level = level;
+    }
+    public int getLevel() {return _level;}
+
+    public boolean permission(Role role) {return this._level >= role.getLevel();}
+}
