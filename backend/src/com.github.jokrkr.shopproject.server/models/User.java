475903a7906a4e02628ff342566a8e642782f6ae
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-15 15:23:50 +0200

    Update User.java
    formatting change, to be more readable

--- backend/src/com.github.jokrkr.shopproject.server/models/User.java
+++ backend/src/com.github.jokrkr.shopproject.server/models/User.java
@@ -3,7 +3,6 @@ package com.github.jokrkr.shopproject.server.models;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-
 public class User {
     private final String _userName;
     private final String _passwordHash;
@@ -17,19 +16,21 @@ public class User {
 
     private String HashPassword(String password) {
         try {
- MessageDigest md =MessageDigest.getInstance("SHA-256");
- byte[] hash = md.digest(password.getBytes());
- StringBuilder hexString = new StringBuilder();
- for (byte b : hash) {
-     hexString.append(String.format("%02X", b));
- }
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                hexString.append(String.format("%02X", b));
+            }
 
- return hexString.toString();
+            return hexString.toString();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("error while hashing password", e);
         }
     }
 
+    //------------------------
+    //general getters
     public String getUserName() {return _userName;}
     public String getPassword() {return _passwordHash;}
     public Role getRole() {return _role;}
