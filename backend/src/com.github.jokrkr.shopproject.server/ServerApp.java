63b70711616cb617a8e2718f45e80643a57238d8
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-15 09:05:04 +0200

    login start
    login  related files and context

--- backend/src/com.github.jokrkr.shopproject.server/ServerApp.java
+++ backend/src/com.github.jokrkr.shopproject.server/ServerApp.java
@@ -1,7 +1,8 @@
 package com.github.jokrkr.shopproject.server;
 
-import com.github.jokrkr.shopproject.server.controllers.ItemController;
-import com.github.jokrkr.shopproject.server.controllers.UserController;
+import com.github.jokrkr.shopproject.server.requestHandler.ItemHandler;
+import com.github.jokrkr.shopproject.server.requestHandler.UserHandler;
+import com.github.jokrkr.shopproject.server.requestHandler.LoginHandler;
 import com.sun.net.httpserver.HttpServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +19,11 @@ public class ServerApp {
         InetSocketAddress address = new InetSocketAddress("127.0.0.1", 8080);
         // creating server
         HttpServer server = HttpServer.create(address, 0); // reminder backlog is related to amount of incoming requests
-        server.createContext("/items", new ItemController()); // context/endpoint
-        server.createContext("/users", new UserController());
+
+        server.createContext("/items", new ItemHandler()); // context/endpoint
+        server.createContext("/users", new UserHandler());
+        server.createContext("/login", new LoginHandler());
+
         server.setExecutor(null);   // basic request handler for the server
         server.start();             // starts the server
         logger.info("Server is listening on port 8080");
