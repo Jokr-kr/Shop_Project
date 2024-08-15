b8ba9f48724bf79051cc66a38210d43cd71ccaf1
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-15 13:38:17 +0200

    Create LogOut.java
    creation of file, not filled out yet

---
+++ backend/src/com.github.jokrkr.shopproject.server/auth/LogOut.java
@@ -0,0 +1,14 @@
+package com.github.jokrkr.shopproject.server.auth;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class LogOut implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+
+    }
+}
