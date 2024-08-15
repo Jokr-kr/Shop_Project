a3b53251b4ce77c31a4e33bf435f4b0f0a1f3724
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-15 13:36:27 +0200

    Update LoginHandler.java
    creation of login handler

---
+++ backend/src/com.github.jokrkr.shopproject.server/requestHandler/LoginHandler.java
@@ -0,0 +1,24 @@
+package com.github.jokrkr.shopproject.server.requestHandler;
+
+import com.github.jokrkr.shopproject.server.auth.LogOut;
+import com.github.jokrkr.shopproject.server.CRUD.UnsupportedMethod;
+import com.github.jokrkr.shopproject.server.auth.LogIn;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class LoginHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        HttpHandler handler = switch (method) {
+            case "GET" -> new LogIn() ;
+            case "DELETE" -> new LogOut();
+            default -> new UnsupportedMethod();
+        };
+        handler.handle(exchange);
+
+    }
+}
