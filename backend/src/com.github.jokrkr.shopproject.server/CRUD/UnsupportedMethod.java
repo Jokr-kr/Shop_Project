c0f8d37832579e77dfe0238fcad8ca1de4582139
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-20 15:27:57 +0200

    Update UnsupportedMethod.java
    changed to use the responseUtil

--- backend/src/com.github.jokrkr.shopproject.server/CRUD/UnsupportedMethod.java
+++ backend/src/com.github.jokrkr.shopproject.server/CRUD/UnsupportedMethod.java
@@ -1,17 +1,14 @@
 package com.github.jokrkr.shopproject.server.CRUD;
 
+import com.github.jokrkr.shopproject.server.response.ResponseUtil;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
-import java.io.OutputStream;
 
 public class UnsupportedMethod implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String response = "Method not supported";
-        exchange.sendResponseHeaders(405, response.length());
-        try (OutputStream os = exchange.getResponseBody()) {
-            os.write(response.getBytes());
+            ResponseUtil.sendResponse(exchange,405,"Method not allowed");
         }
     }
-}
+
