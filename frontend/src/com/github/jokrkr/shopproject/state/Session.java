a9097605871f480e940111276087a620917dc7dd
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-20 16:02:56 +0200

    Create Session.java
    start of client session handling

---
+++ frontend/src/com/github/jokrkr/shopproject/state/Session.java
@@ -0,0 +1,12 @@
+package com.github.jokrkr.shopproject.state;
+
+public class Session {
+    private static Session instance;
+    private String sessionId;
+
+    private Session() {}
+
+    public static Session getInstance() {
+
+    }
+}
