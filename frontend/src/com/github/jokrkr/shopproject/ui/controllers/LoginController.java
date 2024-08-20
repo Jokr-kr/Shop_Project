8e38fe2c667f269b2c8973ea2e7dc818cbf5249b
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-20 15:45:37 +0200

    Update LoginController.java

--- frontend/src/com/github/jokrkr/shopproject/ui/controllers/LoginController.java
+++ frontend/src/com/github/jokrkr/shopproject/ui/controllers/LoginController.java
@@ -1,34 +1,25 @@
 package com.github.jokrkr.shopproject.ui.controllers;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.fxml.FXMLLoader;
 
-import java.io.IOException;
 
 public class LoginController {
+
     @FXML
     private TextField usernameField;
+
     @FXML
     private PasswordField passwordField;
+
     @FXML
     private Label loginResponse;
 
-
     public void TryToLogIn(ActionEvent actionEvent) {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-
-        if (username.isEmpty() || password.isEmpty()) {
-            loginResponse.setText("username or password is incorrect");
-            return;
-        }
 
     }
 }
