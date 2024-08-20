ec8f5061cf2182c4d86844ddf7229b9e41fa7542
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-20 21:34:57 +0200

    Update Session.java
    session management client side

--- frontend/src/com/github/jokrkr/shopproject/state/Session.java
+++ frontend/src/com/github/jokrkr/shopproject/state/Session.java
@@ -7,6 +7,21 @@ public class Session {
     private Session() {}
 
     public static Session getInstance() {
+        if (instance == null) {
+            instance = new Session();
+        }
+        return instance;
+    }
 
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public void clearSession() {
+        this.sessionId = null;
     }
 }
