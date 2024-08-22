3c437f1072cf05b09f8df576ec7ac0d5098cb0c1
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-22 13:08:46 +0200

    Update SceneChanger.java
    changed parameter from Control to Node, making function more versatile

--- frontend/src/com/github/jokrkr/shopproject/utils/SceneChanger.java
+++ frontend/src/com/github/jokrkr/shopproject/utils/SceneChanger.java
@@ -1,25 +1,20 @@
 package com.github.jokrkr.shopproject.utils;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Control;
-import javafx.stage.Stage;
 
 public class SceneChanger {
 
-    public static void changeScene(Control control, String fxmlFile) {
+    public static void changeScene(Node node, String fxmlFile) {
         try {
-
-            Scene currentScene = control.getScene();
-
+            Scene currentScene = node.getScene();
             Parent newRoot = FXMLLoader.load(SceneChanger.class.getResource(fxmlFile));
-
             currentScene.setRoot(newRoot);
-
         } catch (Exception e) {
             e.printStackTrace();
-           //todo better exception handling needed
+            // todo better exception handling needed
         }
     }
 }
