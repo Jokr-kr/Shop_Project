87a9f318fc2a332d5fcf5213d30ae188f161e340
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-22 10:50:11 +0200

    navigation bar and logout
    made it so the top navigation bar is always present,
    also there is a logout button, no functionality for now

--- frontend/src/com/github/jokrkr/shopproject/ui/controllers/MainController.java
+++ frontend/src/com/github/jokrkr/shopproject/ui/controllers/MainController.java
@@ -5,30 +5,32 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 
-import java.io.IOException;
-
 public class MainController {
 
     @FXML
     private StackPane contentPane;
 
-    private void loadPage(String fxmlPath) {
+    @FXML
+    private void showPage1() {loadPage("/com/github/jokrkr/shopproject/ui/views/page1.fxml");}
+    @FXML
+    private void showPage2() {loadPage("/com/github/jokrkr/shopproject/ui/views/page2.fxml");}
+    @FXML
+    private void showPage3() {loadPage("/com/github/jokrkr/shopproject/ui/views/page3.fxml");}
+    @FXML
+    private void showPage4() {loadPage("/com/github/jokrkr/shopproject/ui/views/page4.fxml");}
+
+    private void loadPage(String fxmlFile) {
         try {
-            Parent pane = FXMLLoader.load(getClass().getResource(fxmlPath));
-            contentPane.getChildren().setAll(pane);
-        } catch (IOException e) {
+            Parent page = FXMLLoader.load(getClass().getResource(fxmlFile));
+            contentPane.getChildren().setAll(page);
+        } catch (Exception e) {
             e.printStackTrace();
             //todo better exception handling
         }
     }
 
     @FXML
-    public void showPage1() {loadPage("/com/github/jokrkr/shopproject/ui/views/Page1.fxml");}
-    @FXML
-    public void showPage2() {loadPage("/com/github/jokrkr/shopproject/ui/views/Page2.fxml");}
-    @FXML
-    public void showPage3() {loadPage("/com/github/jokrkr/shopproject/ui/views/Page3.fxml");}
-    @FXML
-    public void showPage4() {loadPage("/com/github/jokrkr/shopproject/ui/views/LoginView.fxml");}
-
+    private void handleLogout() {
+        //todo logout logic
+    }
 }
