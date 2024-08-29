ce7739fa4bb6480cda643bce7895d3fa2e276aff
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-29 13:57:33 +0200

    Update MainController.java
    removing redundant code for better overview, adding manage page and access-control for that page

--- frontend/src/com/github/jokrkr/shopproject/ui/controllers/MainController.java
+++ frontend/src/com/github/jokrkr/shopproject/ui/controllers/MainController.java
@@ -1,37 +1,30 @@
 package com.github.jokrkr.shopproject.ui.controllers;
 
 import com.github.jokrkr.shopproject.state.Session;
-
 import com.github.jokrkr.shopproject.utils.SceneChanger;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.layout.StackPane;
 import com.github.jokrkr.shopproject.utils.ServerCommunication;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
 public class MainController {
-
     @FXML
     private StackPane contentPane;
 
     @FXML
-    private void showPage1() {loadPage("/com/github/jokrkr/shopproject/ui/views/page1.fxml");}
-    @FXML
-    private void showPage2() {loadPage("/com/github/jokrkr/shopproject/ui/views/page2.fxml");}
-    @FXML
-    private void showPage3() {loadPage("/com/github/jokrkr/shopproject/ui/views/page3.fxml");}
-    @FXML
-    private void showPage4() {loadPage("/com/github/jokrkr/shopproject/ui/views/page4.fxml");}
+    private Button manageButton;
 
-    private void loadPage(String fxmlFile) {
-        try {
-            Parent page = FXMLLoader.load(getClass().getResource(fxmlFile));
-            contentPane.getChildren().setAll(page);
-        } catch (Exception e) {
-            e.printStackTrace();
-            //todo better exception handling
+    @FXML
+    public void initialize() {
+        String userRole = Session.getInstance().getUserRole();
+        if (!"admin".equalsIgnoreCase(userRole) && !"moderator".equalsIgnoreCase(userRole)) {
+            manageButton.setDisable(true);
         }
+    }
+
+    @FXML
+    private void showManage() {
+        SceneChanger.changeScene(contentPane, "/com/github/jokrkr/shopproject/ui/views/manage.fxml");
     }
 
     @FXML
