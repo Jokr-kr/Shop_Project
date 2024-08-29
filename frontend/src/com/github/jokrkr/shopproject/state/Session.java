2b7ca707bf058f20df88bf6bb067326dcfa639ca
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-29 14:01:53 +0200

    Update Session.java
    role stored in session for easier lookup,
    
    Security thoughts: if a hacker manage to change the role. they will get acces to the pages on the client-side. server will still realy on the token. which is linked to the original access level

--- frontend/src/com/github/jokrkr/shopproject/state/Session.java
+++ frontend/src/com/github/jokrkr/shopproject/state/Session.java
@@ -3,6 +3,7 @@ package com.github.jokrkr.shopproject.state;
 public class Session {
     private static Session instance;
     private static String sessionId;
+    private static String userRole;
 
     private Session() {}
 
@@ -21,7 +22,16 @@ public class Session {
         Session.sessionId = sessionId;
     }
 
+    public static String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        Session.userRole = userRole;
+    }
+
     public static void clear() {
         sessionId = null;
+        userRole = null;
     }
 }
