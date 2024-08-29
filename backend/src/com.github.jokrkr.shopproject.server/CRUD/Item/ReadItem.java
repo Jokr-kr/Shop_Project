3658b12383103ca4f57b2a9debf76d684c967063
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-29 15:25:02 +0200

    Update ReadItem.java

--- backend/src/com.github.jokrkr.shopproject.server/CRUD/Item/ReadItem.java
+++ backend/src/com.github.jokrkr.shopproject.server/CRUD/Item/ReadItem.java
@@ -3,9 +3,10 @@ package com.github.jokrkr.shopproject.server.CRUD.Item;
 import com.github.jokrkr.shopproject.server.models.Item;
 import com.github.jokrkr.shopproject.server.response.ResponseUtil;
 import com.github.jokrkr.shopproject.server.services.ItemService;
-import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,7 +18,6 @@ import java.util.List;
 
 public class ReadItem implements HttpHandler {
     private static final Logger logger = LoggerFactory.getLogger(ReadItem.class);
-    private final Gson gson = new Gson();
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -25,16 +25,26 @@ public class ReadItem implements HttpHandler {
         try {
             itemService = new ItemService();
             List<Item> items = getItemsFromDatabase(itemService);
-            String jsonResponse = gson.toJson(items);
+            JSONArray jsonResponse = new JSONArray();
 
-            ResponseUtil.sendResponse(exchange, 200, jsonResponse);
+            for (Item item : items) {
+                JSONObject jsonItem = new JSONObject();
+                jsonItem.put("type", item.getType());
+                jsonItem.put("name", item.getName());
+                jsonItem.put("price", item.getPrice());
+                jsonItem.put("quantity", item.getQuantity());
+                jsonItem.put("value", item.getValue());
+                jsonResponse.put(jsonItem);
+            }
+
+            ResponseUtil.sendResponse(exchange, 200, jsonResponse.toString());
 
         } catch (SQLException e) {
             logger.error("Database error", e);
-            ResponseUtil.sendResponse(exchange, 500, "Internal Server Error");
+            ResponseUtil.sendResponse(exchange, 500, new JSONObject().put("error", "Internal Server Error").toString());
         } catch (Exception e) {
             logger.error("Unexpected error", e);
-            ResponseUtil.sendResponse(exchange, 500, "Internal Server Error");
+            ResponseUtil.sendResponse(exchange, 500, new JSONObject().put("error", "Internal Server Error").toString());
         } finally {
             if (itemService != null) {
                 itemService.close();
