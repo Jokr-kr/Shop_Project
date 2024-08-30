e86b5c55d792060b86e443bd8c59309f5ef2bad5
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-08-30 15:01:18 +0200

    removing folder not needed on github

--- out/production/Shop_Project/com.github.jokrkr.shopproject.server/database/CreateUsersTable.sql
+++
@@ -1,9 +0,0 @@
-
-
-CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       username VARCHAR(255) NOT NULL UNIQUE,
-                       password_hash VARCHAR(255) NOT NULL,
-                       role VARCHAR(50) NOT NULL,
-                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
